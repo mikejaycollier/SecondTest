b72c382ac0d28c62b206607d65bf49351f9b7ac9
Mike <mikejaycollier@gmail.com>
2019-08-27 21:04:05 -0600

    first commit

---
+++ SecondTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondTest
+//
+//  Created by Mike Collier on 8/27/19.
+//  Copyright © 2019 Mike Collier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
